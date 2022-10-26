ac606d83f9caa29e20d3a40b21810459a2c76b54
root <godrace062@gmail.com>
2022-10-26 03:44:49 -0700

    Redo Task-0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,7 +13,6 @@ def read_file(filename=""):
         Exception: when the file can be opened
 
     """
-
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
